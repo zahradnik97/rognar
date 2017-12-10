6d37afc176df00de99fa57637cd393df9fa2617c
Richard Ivanek <richard.ivanek@gmail.com>
2017-12-10 22:34:23 +0100

    Finished registrations

--- Rognar/Controllers/HomeController.cs
+++ Rognar/Controllers/HomeController.cs
@@ -29,8 +29,60 @@ namespace Rognar.Controllers
 
         public ActionResult Register()
         {
-            ViewBag.Message = "Register.";
+            bool submit = Request["submit"] != null;
 
+            if (submit)
+            {
+                var db = new Rognar.Models.MainDB();
+                db.Database.Connection.Open();
+                bool success = true;
+                string Username = !string.IsNullOrEmpty(Request["username"]) ? Request["username"].Trim() : string.Empty;
+                string Email = !string.IsNullOrEmpty(Request["email"]) ? Request["email"].Trim() : string.Empty;
+
+                if (string.IsNullOrEmpty(Request["username"]) || Request["username"].Length < 3)
+                {
+                    success = false;
+                    ViewBag.UnameError = "Username must be at least 3 characters long.";
+                }
+                else if (db.Users.Where(u => u.Username.ToLower() == Username.ToLower()).FirstOrDefault() != null)
+                {
+                    success = false;
+                    ViewBag.UnameError = "Username is already taken.";
+                }
+
+                if (string.IsNullOrEmpty(Request["password"]) || Request["password"].Length < 6)
+                {
+                    success = false;
+                    ViewBag.PassError = "Password must be at least 6 inches long.";
+                }
+
+                if (string.IsNullOrEmpty(Request["email"]) || !(new System.ComponentModel.DataAnnotations.EmailAddressAttribute()).IsValid(Request["email"]))
+                {
+                    success = false;
+                    ViewBag.MailError = "Invalid email address.";
+                }
+                else if (db.Users.Where(u => u.Email.ToLower() == Email.ToLower()).FirstOrDefault() != null)
+                {
+                    success = false;
+                    ViewBag.MailError = "Email is already being used.";
+                }
+
+                if (success)
+                {
+                    try
+                    {
+                        db.Users.Add(new Rognar.Models.User() { Username = Request["username"].Trim(), Password = Rognar.Helpers.LoginHelper.HashPassword(Request["password"]), Email = Request["email"] });
+                        db.SaveChanges();
+                        ViewBag.Message = "Account successfully created.";
+                        Response.Redirect("/Home/Index");
+                    }
+                    catch
+                    {
+                        ViewBag.Message = "An error has occurred while creating your account.";
+                        ViewBag.Error = true;
+                    }
+                }
+            }
             return View();
         }
     }
--- Rognar/Models/User.cs
+++ Rognar/Models/User.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Rognar.Models
 {
     public class User
     {
-        public int UserID { get; set; }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }
@@ -16,7 +20,7 @@ namespace Rognar.Models
 
     public class MainDB : DbContext
     {
-        public DbSet<User> Users;
+        public DbSet<User> Users { get; set; }
 
         public MainDB() : base()
         {
